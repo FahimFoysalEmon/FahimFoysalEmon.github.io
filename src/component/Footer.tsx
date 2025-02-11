a29819f7aa5ce8fca45a278045e101bfdc111c2e
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2025-02-11 16:11:07 +0600

    portfolio website done

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-const Footer = () => {
+const Footer: React.FC = () => {
   return (
     <footer className="bg-black text-gray-100 py-6">
       <div className="max-w-7xl mx-auto px-4 flex justify-center items-center">
         <p className="text-sm">
           &copy; {new Date().getFullYear()} Fahim Foysal Emon. All rights reserved.
         </p>
-        
       </div>
     </footer>
   );
